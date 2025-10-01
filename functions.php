b305a67a71a7244dcf28fda5cb99dd7cdb419628
melxcreative <melyhaaly@gmail.com>
2025-10-01 04:14:58 -0600

    Add menu registration to functions.php
    Add Images registration to functions.php
    
    Add theme customize to functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,36 @@
 <?php
 
+// Enable Featured Images
+add_theme_support( 'post-thumbnails' );
+add_theme_support('title-tag');
+
+// Enable Primary Menu
+add_action('after_setup_theme', function () {
+  register_nav_menus([
+    'primary' => 'Primary Menu',
+  ]);
+});
+
 // Enqueue CSS/JS
 add_action('wp_enqueue_scripts', function () {
   wp_enqueue_style('sunshine-style', get_stylesheet_uri());
   wp_enqueue_script('sunshine-script', get_theme_file_uri('scripts/main.js'), [], null, true);
 });
+
+
+function mytheme_customize_register( $wp_customize ) {
+    // Add setting for background color
+    $wp_customize->add_setting( 'background_color_setting', array(
+        'default'   => '#ffffff',
+        'sanitize_callback' => 'sanitize_hex_color',
+        'transport' => 'refresh',
+    ) );
+
+    // Add control for background color
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'background_color_control', array(
+        'label'    => __( 'Background Color', 'mytheme' ),
+        'section'  => 'colors',
+        'settings' => 'background_color_setting',
+    ) ) );
+}
+add_action( 'customize_register', 'mytheme_customize_register' );
