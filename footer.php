50ebf137b4bb9a5a1ad5f870180315d1b1dbe143
melxcreative <melyhaaly@gmail.com>
2025-10-01 04:21:55 -0600

    Simplify footer
    - Minimal footer with copyright and contact info

---
+++ footer.php
@@ -0,0 +1,11 @@
+</main>
+
+<footer class="site-footer">
+    <div class="container">
+        <small>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></small>
+        <small>000-000-0000 &middot; info@sunshinenestdaycare.com</small>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
